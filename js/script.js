b9fe9b570afaeeacef65c395f56cdb766ed650d3
shoubuniao <1920525823@qq.com>
2016-09-27 11:26:09 +0800

    update index

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: anchen
+* @Date:   2016-09-27 11:23:44
+* @Last Modified by:   anchen
+* @Last Modified time: 2016-09-27 11:24:15
+*/
+
+$(document).ready(function(){
+    console.log('hello github!')
+});
